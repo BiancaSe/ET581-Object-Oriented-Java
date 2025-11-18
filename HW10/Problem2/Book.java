2f62d19310b67b5aedf7d27284223a7574f25627
biancase <bia_ny@yahoo.com>
2025-11-18 04:24:52 +0000

    P2final

--- HW10/Problem2/Book.java
+++ HW10/Problem2/Book.java
@@ -1,3 +1,29 @@
-public class Book {
-    
+
+// ET581 Homework 10 Problem 2 - Sort Custom class (Book) , Student: Bianca Serpe, Date: 11/17/2025
+
+
+// Homework 10 - Problem 2
+// Simple Book class implementing Comparable so books sort by title
+
+public class Book implements Comparable<Book> {
+    String title;
+    double price;
+
+    // Constructor to set fields and 
+    public Book(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    // compareTo will sort books alphabetically by title
+    @Override
+    public int compareTo(Book other) {
+        return this.title.compareTo(other.title);
+    }
+
+    // toString makes printing easier
+    public String toString() {
+        return title + " ($" + price + ")";
+    }
 }
+
--- HW10/Problem2/BookSortMain.java
+++ HW10/Problem2/BookSortMain.java
@@ -1,3 +1,31 @@
+
+// ET581 Homework 10 Problem 2 - Sort Custom class (BookSortMain) , Student: Bianca Serpe, Date: 11/17/2025
+
+
+import java.util.*;
+
+
+// Sort books by title using Comparable build-in interface  and the Collections.sort() method
+
 public class BookSortMain {
-    
+    public static void main(String[] args) {
+
+        // Create the list of books
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Java Programming", 39.99));
+        books.add(new Book("Algorithms", 49.50));
+        books.add(new Book("Data Structures", 29.75));
+        books.add(new Book("Operating Systems", 59.00));
+
+      // Sorting the list of Book objects in-place using the natural ordering
+
+        Collections.sort(books);
+
+        // Print in sorted order
+        System.out.println("Sorted by Title (A–Z):");
+        for (Book b : books) {
+            System.out.println(b);
+        }
+    }
 }
+
